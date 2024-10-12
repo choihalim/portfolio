340c120274059f5de529ecc8921d0e5acc4910a7
choihalim <hchoicareer@gmail.com>
2024-10-12 14:48:32 -0500

    fixed dark mode for Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -24,9 +24,9 @@ function Home({ darkMode }) {
                             className={`banner-image ${showBanner ? 'fade-in' : ''}`}
                         />
                         <div className='content'>
-                            <h2>Welcome!</h2>
-                            <h3>My name is Halim, I'm a Full Stack Web Developer</h3>
-                            <Button variant='dark' href="/projects">Check out my Portfolio</Button>
+                            <h2 style={{ color: darkMode ? "#000000" : "#FFFFFF" }}>Hello World!</h2>
+                            <h3 style={{ color: darkMode ? "#000000" : "#FFFFFF" }}>My name is Halim, I'm a Full Stack Web Developer</h3>
+                            <Button variant={darkMode ? 'dark' : 'light'} href="/projects">Check out my Portfolio</Button>
                         </div>
                     </div>
                 </div>
